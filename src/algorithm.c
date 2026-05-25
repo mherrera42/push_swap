0b1cdf232a622be0f40bf985241566a60663e858
mherrera <mherrera@student.42malaga.com>
2026-05-25 17:20:45 +0200

    working on sort 3

--- src/algorithm.c
+++ src/algorithm.c
@@ -6,7 +6,7 @@
 /*   By: mherrera <mherrera@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 13:13:38 by mherrera          #+#    #+#             */
-/*   Updated: 2026/05/25 16:28:08 by mherrera         ###   ########.fr       */
+/*   Updated: 2026/05/25 17:17:22 by mherrera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,16 +26,41 @@ static int	stack_size(t_stack *stack)
 	return (i);
 }
 
-// function that sorts an stack of three numbers
-// static void	sort_three(t_stack **a)
-// {
-	
-// }
+//function that sorts an stack of three numbers
+//hay que comparar por indices, no por numeros... Es decir, necesito una
+//funcion que asigne indices a los numeros en funcion de su ordem
+static void	sort_three(t_stack **stack)
+{
+	//if (*stack == 1 && (*stack)->next == 2 && (*stack)->next->next == 3)
+		//nothing happens
+	if (((*stack)->index == 3 && (*stack)->next->index == 2
+			&& (*stack)->next->next->index == 1))
+	{
+		ra(stack);
+		ra(stack);
+	}
+	else if (((*stack)->index == 2 && (*stack)->next->index == 1
+			&& (*stack)->next->next->index == 3))
+		sa(stack);
+	else if (((*stack)->index == 1 && (*stack)->next->index == 3
+			&& (*stack)->next->next->index == 2))
+	{
+		ra(stack);
+		sa(stack);
+		rra(stack);
+	}
+	else if (((*stack)->index == 3 && (*stack)->next->index == 1
+			&& (*stack)->next->next->index == 2))
+		ra(stack);
+	else if (((*stack)->index == 2 && (*stack)->next->index == 3
+			&& (*stack)->next->next->index == 1))
+		rra(stack);
+}
 
 // //function that sorts an stack of five numbers
 // static void	sort_five(t_stack **a)
 // {
-
+		
 // }
 // //radix sort for more than five numbers
 // static void	radix_sort(t_stack **a)
