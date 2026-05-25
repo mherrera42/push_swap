66df860b94d47d401da983e204ce62add6545ed1
mherrera <mherrera@student.42malaga.com>
2026-05-25 17:25:03 +0200

    still working in sort 3

--- src/algorithm.c
+++ src/algorithm.c
@@ -6,7 +6,7 @@
 /*   By: mherrera <mherrera@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 13:13:38 by mherrera          #+#    #+#             */
-/*   Updated: 2026/05/25 17:17:22 by mherrera         ###   ########.fr       */
+/*   Updated: 2026/05/25 17:24:35 by mherrera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,29 +31,29 @@ static int	stack_size(t_stack *stack)
 //funcion que asigne indices a los numeros en funcion de su ordem
 static void	sort_three(t_stack **stack)
 {
-	//if (*stack == 1 && (*stack)->next == 2 && (*stack)->next->next == 3)
+	//if (*stack == 0 && (*stack)->next == 1 && (*stack)->next->next == 2)
 		//nothing happens
-	if (((*stack)->index == 3 && (*stack)->next->index == 2
-			&& (*stack)->next->next->index == 1))
+	if (((*stack)->index == 2 && (*stack)->next->index == 1
+			&& (*stack)->next->next->index == 0))
 	{
 		ra(stack);
 		ra(stack);
 	}
-	else if (((*stack)->index == 2 && (*stack)->next->index == 1
-			&& (*stack)->next->next->index == 3))
-		sa(stack);
-	else if (((*stack)->index == 1 && (*stack)->next->index == 3
+	else if (((*stack)->index == 1 && (*stack)->next->index == 0
 			&& (*stack)->next->next->index == 2))
+		sa(stack);
+	else if (((*stack)->index == 0 && (*stack)->next->index == 2
+			&& (*stack)->next->next->index == 1))
 	{
 		ra(stack);
 		sa(stack);
 		rra(stack);
 	}
-	else if (((*stack)->index == 3 && (*stack)->next->index == 1
-			&& (*stack)->next->next->index == 2))
-		ra(stack);
-	else if (((*stack)->index == 2 && (*stack)->next->index == 3
+	else if (((*stack)->index == 2 && (*stack)->next->index == 0
 			&& (*stack)->next->next->index == 1))
+		ra(stack);
+	else if (((*stack)->index == 1 && (*stack)->next->index == 2
+			&& (*stack)->next->next->index == 0))
 		rra(stack);
 }
 
